f13872dd9d2101832011b2af4480ac94d2db29c4
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2015-10-20 14:29:40 +0200

    io.load_tif now supports also 2d tif tiles

--- lib/io.py
+++ lib/io.py
@@ -8,5 +8,11 @@ def load_tif(path):
         movie = tif.asarray()
         info['file'] = tif.filename
         info['timestamp'] = tif.pages[0].tags['datetime'].value.decode()
-    info['frames'], info['x size'], info['y size'] = movie.shape
+    if movie.ndim == 2:
+        info['frames'] = 1
+        info['x size'], info['y size'] = movie.shape
+    elif movie.ndim == 3:
+        info['frames'], info['x size'], info['y size'] = movie.shape
+    else:
+        info['shape'] = movie.shape
     return movie, info
