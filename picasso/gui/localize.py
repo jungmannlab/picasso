eefd1adca17abf67f173945b623983e23e7abfea
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2015-10-22 13:07:39 +0200

    added too many files

--- picasso/gui/localize.py
+++ picasso/gui/localize.py
@@ -13,8 +13,8 @@ from PyQt4 import QtCore, QtGui
 from picasso import io, localize
 
 
-GRAYSCALE = [QtGui.qRgb(_, _, _) for _ in range(256)]
-IDENTIFICATION_PARAMETERS_DEFAULTS = {'roi': 5, 'threshold': 100}
+CMAP_GRAYSCALE = [QtGui.qRgb(_, _, _) for _ in range(256)]
+DEFAULT_IDENTIFICATION_PARAMETERS = {'roi': 5, 'threshold': 300}
 
 
 class View(QtGui.QGraphicsView):
@@ -46,12 +46,12 @@ class IdentificationParametersDialog(QtGui.QDialog):
         vbox = QtGui.QVBoxLayout(self)
         grid = QtGui.QGridLayout()
         vbox.addLayout(grid)
-        grid.addWidget(QtGui.QLabel('ROI'), 0, 0)
+        grid.addWidget(QtGui.QLabel('ROI side length:'), 0, 0)
         self.roi_spinbox = OddSpinBox()
         self.roi_spinbox.setValue(parameters['roi'])
         self.roi_spinbox.setSingleStep(2)
         grid.addWidget(self.roi_spinbox, 0, 1)
-        grid.addWidget(QtGui.QLabel('Threshold'), 1, 0)
+        grid.addWidget(QtGui.QLabel('Minimum AGS:'), 1, 0)
         self.threshold_spinbox = QtGui.QSpinBox()
         self.threshold_spinbox.setMaximum(999999999)
         self.threshold_spinbox.setValue(parameters['threshold'])
@@ -69,6 +69,7 @@ class Window(QtGui.QMainWindow):
 
     def __init__(self):
         super().__init__()
+        # Init GUI
         self.setWindowTitle('Picasso: Localize')
         self.resize(768, 768)
         self.init_menu_bar()
@@ -76,9 +77,11 @@ class Window(QtGui.QMainWindow):
         self.setCentralWidget(self.view)
         self.scene = QtGui.QGraphicsScene()
         self.view.setScene(self.scene)
-        self.identification_parameters = IDENTIFICATION_PARAMETERS_DEFAULTS
+        # Init variables
+        self.movie = None
+        self.identification_parameters = DEFAULT_IDENTIFICATION_PARAMETERS
         self.identifications = []
-        self.identification_rectangles = []
+        self.identification_markers = []
 
     def init_menu_bar(self):
         menu_bar = self.menuBar()
@@ -109,13 +112,13 @@ class Window(QtGui.QMainWindow):
         zoom_out_action.setShortcut('Ctrl+-')
         zoom_out_action.triggered.connect(self.on_zoom_out)
         view_menu.addAction(zoom_out_action)
+        fit_in_view_action = view_menu.addAction('Fit image to window')
+        fit_in_view_action.setShortcut('Ctrl+W')
+        fit_in_view_action.triggered.connect(self.fit_in_view)
+        view_menu.addAction(fit_in_view_action)
 
         """ Analyze """
         analyze_menu = menu_bar.addMenu('Analyze')
-        identification_parameters_action = analyze_menu.addAction('Identification parameters')
-        identification_parameters_action.setShortcut('Ctrl+Shift+I')
-        identification_parameters_action.triggered.connect(self.on_identification_parameters)
-        analyze_menu.addAction(identification_parameters_action)
         identify_action = analyze_menu.addAction('Identify')
         identify_action.setShortcut('Ctrl+I')
         identify_action.triggered.connect(self.on_identify)
@@ -126,17 +129,19 @@ class Window(QtGui.QMainWindow):
         if path:
             self.movie, self.info = io.load_raw(path)
             self.set_frame(0)
-            self.view.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
+            self.fit_in_view()
 
     def on_next_frame(self):
-        if self.current_frame_number < self.info['frames']:
+        if self.movie is not None and self.current_frame_number < self.info['frames']:
             self.set_frame(self.current_frame_number + 1)
 
     def on_previous_frame(self):
-        if self.current_frame_number > 0:
+        if self.movie is not None and self.current_frame_number > 0:
             self.set_frame(self.current_frame_number - 1)
 
     def set_frame(self, number):
+        if self.identifications:
+            self.remove_identification_markers()
         self.current_frame_number = number
         frame = self.movie[number]
         frame = frame.astype('float32')
@@ -146,40 +151,45 @@ class Window(QtGui.QMainWindow):
         frame = frame.astype('uint8')
         width, height = frame.shape
         qimage = QtGui.QImage(frame.data, width, height, QtGui.QImage.Format_Indexed8)
-        qimage.setColorTable(GRAYSCALE)
+        qimage.setColorTable(CMAP_GRAYSCALE)
         qpixmap = QtGui.QPixmap.fromImage(qimage)
-        self.scene = QtGui.QGraphicsScene()
+        self.scene = QtGui.QGraphicsScene(self.view)
         self.scene.addPixmap(qpixmap)
         self.view.setScene(self.scene)
         if self.identifications:
-            self.draw_identifications()
+            self.draw_identification_markers()
 
-    def on_identification_parameters(self):
+    def on_identify(self):
+        if self.movie is not None:
+            self.identify()
+        else:
+            self.on_file_open()
+            self.identify()
+
+    def identify(self):
         dialog = IdentificationParametersDialog(self.identification_parameters, self)
         result = dialog.exec_()
         if result == QtGui.QDialog.Accepted:
             self.identification_parameters = dialog.parameters()
+            self.identifications = localize.identify(self.movie, *self.identification_parameters.values())
+            print(self.identifications)
+            self.draw_identification_markers()
 
-    def on_identify(self):
-        roi_size = self.identification_parameters['roi']
-        threshold = self.identification_parameters['threshold']
-        self.identifications = []
-        for frame in self.movie:
-            frame_identificiations = localize.identify(frame, roi_size, threshold)
-            self.identifications.append(frame_identificiations)
-        self.draw_identifications()
-
-    def draw_identifications(self):
-        # Remove old identification
-        for rect in self.identification_rectangles:
+    def remove_identification_markers(self):
+        for rect in self.identification_markers:
             self.scene.removeItem(rect)
-        frame_identifications = self.identifications[self.current_frame_number]
-        self.identification_rectangles = []
-        roi_size = self.identification_parameters['roi']
-        roi_size_half = int(roi_size/2)
-        for y, x in frame_identifications:
-            rect = self.scene.addRect(x - roi_size_half, y - roi_size_half, roi_size, roi_size, QtGui.QPen(QtGui.QColor('red')))
-            self.identification_rectangles.append(rect)
+        self.identification_markers = []
+
+    def draw_identification_markers(self):
+        identifications_frame = self.identifications[self.current_frame_number]
+        roi, threshold = self.identification_parameters.values()
+        roi_half = int(roi/2)
+        for y, x in identifications_frame:
+            rect = self.scene.addRect(x - roi_half, y - roi_half, roi, roi, QtGui.QPen(QtGui.QColor('red')))
+            self.identification_markers.append(rect)
+
+    def fit_in_view(self):
+        self.view.fitInView(self.scene.sceneRect(), QtCore.Qt.KeepAspectRatio)
 
     def on_zoom_in(self):
         self.view.scale(10 / 7, 10 / 7)
--- picasso/localize.py
+++ picasso/localize.py
@@ -2,7 +2,7 @@
     picasso.localize
     ~~~~~~~~~~~~~~~~
 
-    Identify and localize fluorescent single molecules in an image sequence
+    Identify and localize fluorescent single molecules in an frame sequence
 
     :author: Joerg Schnitzbauer, 2015
 """
@@ -12,74 +12,45 @@ import numba
 
 
 @numba.jit(nopython=True)
-def local_maxima_map(image, roi_size):
+def local_maxima_map(frame, roi):
     """ Finds pixels with maximum value within a region of interest """
-    X, Y = image.shape
-    maxima_map = np.zeros_like(image)
-    roi_half_size = int(np.floor(roi_size/2))
-    for i in range(roi_half_size+1, X-roi_half_size-2):
-        for j in range(roi_half_size+1, Y-roi_half_size-2):
-            local_image = image[i-roi_half_size:i+roi_half_size+1, j-roi_half_size:j+roi_half_size+1]
-            flat_max = np.argmax(local_image)
-            j_local_max = int(flat_max / roi_size)
-            i_local_max = int(flat_max % roi_size)
-            if (i_local_max == roi_half_size) and (j_local_max == roi_half_size):
+    X, Y = frame.shape
+    maxima_map = np.zeros_like(frame)
+    roi_half = int(roi/2)
+    for i in range(roi, X-roi):
+        for j in range(roi, Y-roi):
+            local_frame = frame[i-roi_half:i+roi_half+1, j-roi_half:j+roi_half+1]
+            flat_max = np.argmax(local_frame)
+            j_local_max = int(flat_max / roi)
+            i_local_max = int(flat_max % roi)
+            if (i_local_max == roi_half) and (j_local_max == roi_half):
                 maxima_map[i, j] = 1
     return maxima_map
 
 
-def local_maxima(image, roi_size):
-    maxima_map = local_maxima_map(image, roi_size)
-    x, y = np.where(maxima_map)
-    return np.vstack((x, y)).T
-
-
 @numba.jit(nopython=True)
-def find_peaks(image, roi_size, abs_gradient):
-    X, Y = image.shape
+def spot_map(frame, roi, abs_gradient):
+    """ Returns the sum of the absolute gradient within a ROI around each pixel """
+    X, Y = frame.shape
     spot_map = np.zeros_like(abs_gradient)
-    roi_size_half = np.int(np.floor(roi_size/2))
-    for i in range(roi_size_half+1, X-roi_size_half-2):
-        for j in range(roi_size_half+1, Y-roi_size_half-2):
-            local_gradient = abs_gradient[i-roi_size_half:i+roi_size_half+1, j-roi_size_half:j+roi_size_half+1]
+    roi_half = int(roi/2)
+    for i in range(roi, X-roi):
+        for j in range(roi, Y-roi):
+            local_gradient = abs_gradient[i-roi_half:i+roi_half+1, j-roi_half:j+roi_half+1]
             spot_map[i, j] = np.sum(local_gradient)
     return spot_map
 
 
-@numba.jit(nopython=True)
-def argmax2d_subset(image, candidate_coordinates):
-    max_x = candidate_coordinates[0][0]
-    max_y = candidate_coordinates[0][1]
-    max = image[max_x, max_y]
-    n_candidates = candidate_coordinates.shape[0]
-    for i in range(1, n_candidates):
-        x_candidate = candidate_coordinates[i, 0]
-        y_candidate = candidate_coordinates[i, 1]
-        if image[x_candidate, y_candidate] > max:
-            max = image[x_candidate, y_candidate]
-            max_x = x_candidate
-            max_y = y_candidate
-    return max_x, max_y
-
-
-def identify(image, roi_size, threshold):
-    gradient_x, gradient_y = np.gradient(np.float32(image))
+def identify_frame(frame, roi, threshold):
+    gradient_x, gradient_y = np.gradient(np.float32(frame))
     abs_gradient = np.sqrt(gradient_x**2 + gradient_y**2)
-    spot_map = find_peaks(image, roi_size, abs_gradient)
-    lm_map = local_maxima_map(image, roi_size)
-    spot_map_thesholded = spot_map > threshold
-    combined_map = (lm_map * spot_map_thesholded) > 0.5
+    s_map = spot_map(frame, roi, abs_gradient)
+    lm_map = local_maxima_map(frame, roi)
+    s_map_thesholded = s_map > threshold
+    combined_map = (lm_map * s_map_thesholded) > 0.5
     return np.vstack(np.where(combined_map)).T
-    """
-    labeled_spot_map, n_labels = label(spot_map > threshold)
-    x = np.zeros(n_labels, dtype='uint16')
-    y = np.zeros(n_labels, dtype='uint16')
-    for i in range(n_labels):
-        spot_coordinates = np.vstack(np.where(labeled_spot_map == i)).T
-        x[i], y[i] = argmax2d_subset(image, spot_coordinates)
-    return np.vstack((x, y)).T, spot_map
-    """
 
 
-def localize(files, parameters):
-    print('Localizer started')
+def identify(movie, roi, threshold):
+    identifications = [identify_frame(frame, roi, threshold) for frame in movie]
+    return identifications
