9658d9eb9c327add7d5b8d808c2e28aa8128d196
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2016-02-04 09:34:18 +0100

    moved xcorr_fft to imageprocess

--- picasso/lib.py
+++ picasso/lib.py
@@ -25,10 +25,6 @@ def calculate_optimal_bins(data, max_n_bins=None):
     return _np.linspace(bin_min, data.max(), n_bins)
 
 
-def xcorr_fft(A, B):
-    return _np.fft.fftshift(_np.real(_np.fft.ifft2(_np.fft.fft2(A)*_np.conj(_np.fft.fft2(B)))))
-
-
 def append_to_rec(rec_array, data, name):
     return _append_fields(rec_array, name, data, dtypes=data.dtype, usemask=False, asrecarray=True)
 
