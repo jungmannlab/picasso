31ee19e9f52afe1093b97ad32132203681727de0
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2016-02-11 08:57:26 +0100

    removes splot

--- picasso/imageprocess.py
+++ picasso/imageprocess.py
@@ -17,40 +17,6 @@ import lmfit as _lmfit
 _plt.style.use('ggplot')
 
 
-def split(movie, info, frame=0, vmax=0.9, rectangle=None):
-    if rectangle is None:
-        subregions = []
-        shifts = []
-        infos = []
-
-        def on_split_select(press_event, release_event):
-            x1, y1 = press_event.xdata, press_event.ydata
-            x2, y2 = release_event.xdata, release_event.ydata
-            xmin = int(min(x1, x2) + 0.5)
-            xmax = int(max(x1, x2) + 0.5)
-            ymin = int(min(y1, y2) + 0.5)
-            ymax = int(max(y1, y2) + 0.5)
-            subregions.append(movie[:, ymin:ymax+1, xmin:xmax+1])
-            shifts.append((ymin, xmin))
-            subregion_info = _copy.deepcopy(info)
-            subregion_info[0]['Height'] = ymax - ymin + 1
-            subregion_info[0]['Width'] = xmax - xmin + 1
-            infos.append(subregion_info)
-
-        f = _plt.figure(figsize=(12, 12))
-        ax = f.add_subplot(111)
-        ax.matshow(movie[frame], cmap='viridis', vmax=vmax*movie[frame].max())
-        ax.grid(False)
-        selector = _RectangleSelector(ax, on_split_select, useblit=True, rectprops=dict(edgecolor='red', fill=False))
-        _plt.show()
-        wavelengths = []
-        for i in range(len(shifts)):
-            wavelengths.append(int(input('Excitation wavelength for region {}? '.format(i))))
-        return subregions, shifts, infos, wavelengths
-    else:
-        pass
-
-
 def xcorr(imageA, imageB):
     FimageA = _fft.fft2(imageA)
     CFimageB = _np.conj(_fft.fft2(imageB))
