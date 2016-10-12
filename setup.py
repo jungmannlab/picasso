2e7efa21985e5855ba0618bdcd0fe4b4b6803bea
Joerg Schnitzbauer <joschnitzbauer@gmail.com>
2016-10-12 15:27:04 +0200

    added paint_sequences.csv to setup

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
 setup(
-    name = 'picasso',
-    version = '0.1',
-    author = 'Joerg Schnitzbauer',
-    author_email = 'joschnitzbauer@gmail.com',
-    url = 'https://gitlab.com/jungmannlab/picasso',
-    packages = ['picasso', 'picasso.gui'],
-    classifiers = ["Programming Language :: Python",
-                   "Programming Language :: Python :: 3.5",
-                   "License :: OSI Approved :: MIT License",
-                   "Operating System :: OS Independent"],
-    package_data = {'picasso': ['gui/icons/*.ico', 'config_template.yaml', 'base_sequences.csv']}
+    name='picasso',
+    version='0.1',
+    author='Joerg Schnitzbauer',
+    author_email='joschnitzbauer@gmail.com',
+    url='https://gitlab.com/jungmannlab/picasso',
+    packages=['picasso', 'picasso.gui'],
+    classifiers=["Programming Language :: Python",
+                 "Programming Language :: Python :: 3.5",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent"],
+    package_data={'picasso': ['gui/icons/*.ico',
+                              'config_template.yaml',
+                              'base_sequences.csv',
+                              'paint_sequences.csv']}
 )
