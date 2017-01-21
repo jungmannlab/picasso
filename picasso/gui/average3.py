b3f8720bab7733add7dd73f85a77b24830a8d340
Maximilian Strauss <mstrauss@biochem.mpg.de>
2017-01-21 22:39:40 +0100

    implemented some more features

--- picasso/gui/average3.py
+++ picasso/gui/average3.py
@@ -24,6 +24,12 @@ from PyQt4 import QtCore, QtGui
 
 from .. import io, lib, render
 
+
+DEFAULT_OVERSAMPLING = 1.0
+INITIAL_REL_MAXIMUM = 0.5
+ZOOM = 10 / 7
+N_GROUP_COLORS = 8
+
 @numba.jit(nopython=True, nogil=True)
 def render_hist(x, y, oversampling, t_min, t_max):
     n_pixel = int(np.ceil(oversampling * (t_max - t_min)))
@@ -283,6 +289,7 @@ class Window(QtGui.QMainWindow):
         icon_path = os.path.join(this_directory, 'icons', 'average.ico')
         icon = QtGui.QIcon(icon_path)
         self.setWindowIcon(icon)
+        self.setAcceptDrops(True)
         #self.view = View(self)
         #self.setCentralWidget(self.view)
         #self.parameters_dialog = ParametersDialog(self)
@@ -305,17 +312,36 @@ class Window(QtGui.QMainWindow):
         #average_action.triggered.connect(self.view.average)
         self.status_bar = self.statusBar()
 
-        #Create groupboxes etc.
+        self._pixmap = None
+        self.locs = []
+        self.infos = []
+        self.locs_paths = []
+        self._mode = 'Zoom'
+        self._pan = False
+        self._size_hint = (768, 768)
+        self.n_locs = 0
+        self._picks = []
+        self.index_blocks = []
+        self._drift = []
+
+
+        #Define DisplaySettingsDialog
+        viewxy = QtGui.QLabel('XY')
+        viewxz = QtGui.QLabel('XZ')
+        viewyz = QtGui.QLabel('YZ')
+        viewcp = QtGui.QLabel('Convergence Plot')
+
+        # Define layout
         display_groupbox = QtGui.QGroupBox('Display')
         displaygrid = QtGui.QGridLayout(display_groupbox)
-        displaygrid.addWidget(QtGui.QLabel('XY'), 0, 0)
-        displaygrid.addWidget(QtGui.QLabel('XZ'), 1, 0)
-        displaygrid.addWidget(QtGui.QLabel('YZ'), 0, 1)
-        displaygrid.addWidget(QtGui.QLabel('Convergence Plot'), 1, 1)
+        displaygrid.addWidget(viewxy, 0, 0)
+        displaygrid.addWidget(viewxz, 1, 0)
+        displaygrid.addWidget(viewyz, 0, 1)
+        displaygrid.addWidget(viewcp, 1, 1)
 
         button_groupbox = QtGui.QGroupBox('Buttons')
         buttongrid = QtGui.QGridLayout(button_groupbox)
-        #add all buttons etc.
+
         rotation_groupbox = QtGui.QGroupBox('Rotation + Translation')
         rotationgrid = QtGui.QGridLayout(rotation_groupbox)
         centerofmassbtn = QtGui.QPushButton("Center of Mass XYZ")
@@ -353,20 +379,14 @@ class Window(QtGui.QMainWindow):
         deggrid.addWidget(full_degbtn,0,0)
         deggrid.addWidget(part_degbtn,0,1)
 
-
-
         #Rotation Groupbox
-
         rotationgrid.addWidget(axis_groupbox,0,0)
         rotationgrid.addWidget(proj_groupbox,1,0)
         rotationgrid.addWidget(deg_groupbox,2,0)
         rotationgrid.addWidget(rotatebtn,3,0)
 
-
-
         buttongrid.addWidget(centerofmassbtn,0,0)
         buttongrid.addWidget(rotation_groupbox,1,0)
-
 
         translatexbtn = QtGui.QPushButton("Translate X")
         translateybtn = QtGui.QPushButton("Translate Y")
@@ -389,29 +409,20 @@ class Window(QtGui.QMainWindow):
 
         buttongrid.addWidget(operate_groupbox,2,0)
 
-
-
         self.grid = QtGui.QGridLayout()
         self.grid.addWidget(display_groupbox, 0, 0,2,1)
         self.grid.addWidget(button_groupbox,0,1,1,1)
 
-
-
         mainWidget = QtGui.QWidget()
         mainWidget.setLayout(self.grid)
         self.setCentralWidget(mainWidget)
-
         self.status_bar.showMessage('Average3 ready.')
-
-        mainWidget = QtGui.QWidget()
-        mainWidget.setLayout(self.grid)
-        self.setCentralWidget(mainWidget)
 
 
     def open(self):
         path = QtGui.QFileDialog.getOpenFileName(self, 'Open localizations', filter='*.hdf5')
         if path:
-            self.view.open(path)
+            self.openhdf(path)
 
     def save(self):
         out_path = os.path.splitext(self.view.path)[0] + '_avg.hdf5'
@@ -419,6 +430,200 @@ class Window(QtGui.QMainWindow):
         if path:
             self.view.save(path)
 
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasUrls():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        urls = event.mimeData().urls()
+        path = urls[0].toLocalFile()
+        ext = os.path.splitext(path)[1].lower()
+        if ext == '.hdf5':
+            print('Opening')
+            self.add(path)
+
+    def add(self, path, render=True):
+        try:
+            locs, info = io.load_locs(path, qt_parent=self)
+        except io.NoMetadataFileError:
+            return
+        locs = lib.ensure_sanity(locs, info)
+        self.locs.append(locs)
+        self.infos.append(info)
+        self.locs_paths.append(path)
+        self.index_blocks.append(None)
+        self._drift.append(None)
+        if len(self.locs) == 1:
+            self.median_lp = np.mean([np.median(locs.lpx), np.median(locs.lpy)])
+            if hasattr(locs, 'group'):
+                groups = np.unique(locs.group)
+                np.random.shuffle(groups)
+                groups %= N_GROUP_COLORS
+                self.group_color = groups[locs.group]
+            if render:
+                self.fit_in_view(autoscale=True)
+        else:
+            if render:
+                self.update_scene()
+        os.chdir(os.path.dirname(path))
+
+    def fit_in_view(self, autoscale=False):
+        movie_height, movie_width = self.movie_size()
+        viewport = [(0, 0), (movie_height, movie_width)]
+        self.update_scene(viewport=viewport, autoscale=autoscale)
+
+    def movie_size(self):
+        movie_height = self.max_movie_height()
+        movie_width = self.max_movie_width()
+        return (movie_height, movie_width)
+
+    def max_movie_height(self):
+        ''' Returns maximum height of all loaded images. '''
+        return max(info[0]['Height'] for info in self.infos)
+
+    def max_movie_width(self):
+        return max([info[0]['Width'] for info in self.infos])
+
+    def update_scene(self, viewport=None, autoscale=False, use_cache=False, picks_only=False):
+        n_channels = len(self.locs)
+        if n_channels:
+            viewport = viewport or self.viewport
+            self.draw_scene(viewport, autoscale=autoscale, use_cache=use_cache, picks_only=picks_only)
+            #self.update_cursor()
+
+    def draw_scene(self, viewport, autoscale=False, use_cache=False, picks_only=False):
+        if not picks_only:
+            self.viewport = self.adjust_viewport_to_view(viewport)
+            qimage = self.render_scene(autoscale=autoscale, use_cache=use_cache)
+            qimage = qimage.scaled(self.width(), self.height(), QtCore.Qt.KeepAspectRatioByExpanding)
+            self.qimage_no_picks = self.draw_scalebar(qimage)
+            dppvp = self.display_pixels_per_viewport_pixels()
+            self.window.display_settings_dialog.set_zoom_silently(dppvp)
+        self.qimage = self.draw_picks(self.qimage_no_picks)
+        pixmap = QtGui.QPixmap.fromImage(self.qimage)
+        self.setPixmap(pixmap)
+        self.window.update_info()
+
+    def adjust_viewport_to_view(self, viewport):
+        ''' Adds space to a desired viewport so that it matches the window aspect ratio. '''
+        viewport_height = viewport[1][0] - viewport[0][0]
+        viewport_width = viewport[1][1] - viewport[0][1]
+        view_height = self.height()
+        view_width = self.width()
+        viewport_aspect = viewport_width / viewport_height
+        view_aspect = view_width / view_height
+        if view_aspect >= viewport_aspect:
+            y_min = viewport[0][0]
+            y_max = viewport[1][0]
+            x_range = viewport_height * view_aspect
+            x_margin = (x_range - viewport_width) / 2
+            x_min = viewport[0][1] - x_margin
+            x_max = viewport[1][1] + x_margin
+        else:
+            x_min = viewport[0][1]
+            x_max = viewport[1][1]
+            y_range = viewport_width / view_aspect
+            y_margin = (y_range - viewport_height) / 2
+            y_min = viewport[0][0] - y_margin
+            y_max = viewport[1][0] + y_margin
+        return [(y_min, x_min), (y_max, x_max)]
+
+    def render_scene(self, autoscale=False, use_cache=False, cache=True, viewport=None):
+        kwargs = self.get_render_kwargs(viewport=viewport)
+        n_channels = len(self.locs)
+        if n_channels == 1:
+            self.render_single_channel(kwargs, autoscale=autoscale, use_cache=use_cache, cache=cache)
+        else:
+            self.render_multi_channel(kwargs, autoscale=autoscale, use_cache=use_cache, cache=cache)
+        self._bgra[:, :, 3].fill(255)
+        Y, X = self._bgra.shape[:2]
+        qimage = QtGui.QImage(self._bgra.data, X, Y, QtGui.QImage.Format_RGB32)
+        return qimage
+
+
+    def get_render_kwargs(self, viewport=None):  #Dummy for now: TODO: Implement
+        viewport = [(0, 0), (256, 256)]
+        return {'oversampling': 20,
+            'viewport': viewport,
+            'blur_method': None,
+            'min_blur_width': float(0)}
+
+
+
+    def get_render_kwargs2(self, viewport=None):
+        ''' Returns a dictionary to be used for the keyword arguments of render. '''
+        blur_button = self.window.display_settings_dialog.blur_buttongroup.checkedButton()
+        optimal_oversampling = self.display_pixels_per_viewport_pixels()
+        if self.window.display_settings_dialog.dynamic_oversampling.isChecked():
+            oversampling = optimal_oversampling
+            self.window.display_settings_dialog.set_oversampling_silently(optimal_oversampling)
+        else:
+            oversampling = float(self.window.display_settings_dialog.oversampling.value())
+            if oversampling > optimal_oversampling:
+                QtGui.QMessageBox.information(self,
+                                              'Oversampling too high',
+                                              'Oversampling will be adjusted to match the display pixel density.')
+                oversampling = optimal_oversampling
+                self.window.display_settings_dialog.set_oversampling_silently(optimal_oversampling)
+        if viewport is None:
+            viewport = self.viewport
+        return {'oversampling': oversampling,
+                'viewport': viewport,
+                'blur_method': self.window.display_settings_dialog.blur_methods[blur_button],
+                'min_blur_width': float(self.window.display_settings_dialog.min_blur_width.value())}
+
+    def render_multi_channel(self, kwargs, autoscale=False, locs=None, use_cache=False, cache=True):
+        if locs is None:
+            locs = self.locs
+        n_channels = len(locs)
+        hues = np.arange(0, 1, 1 / n_channels)
+        colors = [colorsys.hsv_to_rgb(_, 1, 1) for _ in hues]
+        if use_cache:
+            n_locs = self.n_locs
+            image = self.image
+        else:
+            renderings = [render.render(_, **kwargs) for _ in locs]
+            n_locs = sum([_[0] for _ in renderings])
+            image = np.array([_[1] for _ in renderings])
+        if cache:
+            self.n_locs = n_locs
+            self.image = image
+        image = self.scale_contrast(image)
+        Y, X = image.shape[1:]
+        bgra = np.zeros((Y, X, 4), dtype=np.float32)
+        for color, image in zip(colors, image):
+            bgra[:, :, 0] += color[2] * image
+            bgra[:, :, 1] += color[1] * image
+            bgra[:, :, 2] += color[0] * image
+        bgra = np.minimum(bgra, 1)
+        self._bgra = self.to_8bit(bgra)
+        return self._bgra
+
+    def render_single_channel(self, kwargs, autoscale=False, use_cache=False, cache=True):
+        locs = self.locs[0]
+        if hasattr(locs, 'group'):
+            locs = [locs[self.group_color == _] for _ in range(N_GROUP_COLORS)]
+            return self.render_multi_channel(kwargs, autoscale=autoscale, locs=locs, use_cache=use_cache)
+        if use_cache:
+            n_locs = self.n_locs
+            image = self.image
+        else:
+            n_locs, image = render.render(locs, **kwargs)
+        if cache:
+            self.n_locs = n_locs
+            self.image = image
+        image = self.scale_contrast(image, autoscale=autoscale)
+        image = self.to_8bit(image)
+        Y, X = image.shape
+        cmap = self.window.display_settings_dialog.colormap.currentText()
+        cmap = np.uint8(np.round(255 * plt.get_cmap(cmap)(np.arange(256))))
+        self._bgra = np.zeros((Y, X, 4), dtype=np.uint8, order='C')
+        self._bgra[..., 0] = cmap[:, 2][image]
+        self._bgra[..., 1] = cmap[:, 1][image]
+        self._bgra[..., 2] = cmap[:, 0][image]
+        return self._bgra
 
 def main():
 
