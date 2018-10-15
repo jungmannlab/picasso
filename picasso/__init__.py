6b15acf9bafd529d4fee970c01f515ff747e645f
straussmaximilian <straussmaximilian@gmail.com>
2018-10-15 10:40:29 +0200

    codestyle changes

--- picasso/__init__.py
+++ picasso/__init__.py
@@ -2,17 +2,17 @@
     picasso/__init__.py
     ~~~~~~~~~~~~~~~~~~~~
 
-    :author: Joerg Schnitzbauer, 2015
-    :copyright: Copyright (c) 2015 Jungmann Lab, Max Planck Institute of Biochemistry
+    :author: Joerg Schnitzbauer, Maximilian Thomas Strauss, 2016-2018
+    :copyright: Copyright (c) 2016-2018 Jungmann Lab, MPI of Biochemistry
 """
 import os.path as _ospath
 import yaml as _yaml
 
 
 _this_file = _ospath.abspath(__file__)
-_this_directory = _ospath.dirname(_this_file)
+_this_dir = _ospath.dirname(_this_file)
 try:
-    with open(_ospath.join(_this_directory, 'config.yaml'), 'r') as config_file:
+    with open(_ospath.join(_this_dir, 'config.yaml'), 'r') as config_file:
         CONFIG = _yaml.load(config_file)
     if CONFIG is None:
         CONFIG = {}
