47cb39f2e96df5382ed73a03ae3a1eb04264405b
straussmaximilian <straussmaximilian@gmail.com>
2018-10-24 22:48:58 +0200

    black commit

--- picasso/lib.py
+++ picasso/lib.py
@@ -26,9 +26,10 @@ _dialogs = []
 
 
 class ProgressDialog(QtGui.QProgressDialog):
-
     def __init__(self, description, minimum, maximum, parent):
-        super().__init__(description, None, minimum, maximum, parent, QtCore.Qt.CustomizeWindowHint)
+        super().__init__(
+            description, None, minimum, maximum, parent, QtCore.Qt.CustomizeWindowHint
+        )
         _dialogs.append(self)
         self.setMinimumDuration(500)
         self.setModal(True)
@@ -43,7 +44,6 @@ class ProgressDialog(QtGui.QProgressDialog):
 
 
 class StatusDialog(QtGui.QDialog):
-
     def __init__(self, description, parent):
         super(StatusDialog, self).__init__(parent, QtCore.Qt.CustomizeWindowHint)
         _dialogs.append(self)
@@ -58,12 +58,13 @@ class StatusDialog(QtGui.QDialog):
 
 
 class AutoDict(_collections.defaultdict):
-    '''
+    """
     A defaultdict whose auto-generated values are defaultdicts itself.
     This allows for auto-generating nested values, e.g.
     a = AutoDict()
     a['foo']['bar']['carrot'] = 42
-    '''
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(AutoDict, *args, **kwargs)
 
@@ -79,7 +80,7 @@ def cancel_dialogs():
 
 
 def cumulative_exponential(x, a, t, c):
-    return a * (1 - _np.exp(-(x/t))) + c
+    return a * (1 - _np.exp(-(x / t))) + c
 
 
 CumulativeExponentialModel = _Model(cumulative_exponential)
@@ -87,8 +88,8 @@ CumulativeExponentialModel = _Model(cumulative_exponential)
 
 def calculate_optimal_bins(data, max_n_bins=None):
     iqr = _np.subtract(*_np.percentile(data, [75, 25]))
-    bin_size = 2 * iqr * len(data)**(-1/3)
-    if data.dtype.kind in ('u', 'i') and bin_size < 1:
+    bin_size = 2 * iqr * len(data) ** (-1 / 3)
+    if data.dtype.kind in ("u", "i") and bin_size < 1:
         bin_size = 1
     bin_min = data.min() - bin_size / 2
     n_bins = _np.ceil((data.max() - bin_min) / bin_size)
@@ -104,18 +105,22 @@ def calculate_optimal_bins(data, max_n_bins=None):
 def append_to_rec(rec_array, data, name):
     if hasattr(rec_array, name):
         rec_array = remove_from_rec(rec_array, name)
-    return _append_fields(rec_array, name, data, dtypes=data.dtype, usemask=False, asrecarray=True)
+    return _append_fields(
+        rec_array, name, data, dtypes=data.dtype, usemask=False, asrecarray=True
+    )
     return rec_array
 
 
 def ensure_sanity(locs, info):
     # no inf or nan:
-    locs = locs[_np.all(_np.array([_np.isfinite(locs[_]) for _ in locs.dtype.names]), axis=0)]
+    locs = locs[
+        _np.all(_np.array([_np.isfinite(locs[_]) for _ in locs.dtype.names]), axis=0)
+    ]
     # other sanity checks:
     locs = locs[locs.x > 0]
     locs = locs[locs.y > 0]
-    locs = locs[locs.x < info[0]['Width']]
-    locs = locs[locs.y < info[0]['Height']]
+    locs = locs[locs.x < info[0]["Width"]]
+    locs = locs[locs.y < info[0]["Height"]]
     locs = locs[locs.lpx > 0]
     locs = locs[locs.lpy > 0]
     return locs
@@ -124,8 +129,8 @@ def ensure_sanity(locs, info):
 def is_loc_at(x, y, locs, r):
     dx = locs.x - x
     dy = locs.y - y
-    r2 = r**2
-    return dx**2 + dy**2 < r2
+    r2 = r ** 2
+    return dx ** 2 + dy ** 2 < r2
 
 
 def locs_at(x, y, locs, r):
@@ -141,7 +146,7 @@ def minimize_shifts(shifts_x, shifts_y, shifts_z=None):
     A = _np.zeros((n_pairs, n_channels - 1))
     flag = 0
     for i in range(n_channels - 1):
-        for j in range(i+1, n_channels):
+        for j in range(i + 1, n_channels):
             rij[flag, 0] = shifts_y[i, j]
             rij[flag, 1] = shifts_x[i, j]
             if n_dims == 3:
@@ -166,20 +171,20 @@ def remove_from_rec(rec_array, name):
     return _drop_fields(rec_array, name, usemask=False, asrecarray=True)
 
 
-def locs_glob_map(func, pattern, args=[], kwargs={}, extension=''):
-    '''
+def locs_glob_map(func, pattern, args=[], kwargs={}, extension=""):
+    """
     Maps a function to localization files, specified by a unix style path pattern.
     The function must take two arguments: locs and info. It may take additional args and kwargs which
     are supplied to this map function.
     A new locs file will be saved if an extension is provided. In that case the mapped function must return
     new locs and a new info dict.
-    '''
+    """
     paths = _glob.glob(pattern)
     for path in paths:
         locs, info = _io.load_locs(path)
         result = func(locs, info, path, *args, **kwargs)
         if extension:
             base, ext = _ospath.splitext(path)
-            out_path = base + '_' + extension + '.hdf5'
+            out_path = base + "_" + extension + ".hdf5"
             locs, info = result
             _io.save_locs(out_path, locs, info)
