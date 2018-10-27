5f4a644815000fc7bc976d5b98076a6284fe49bd
straussmaximilian <straussmaximilian@gmail.com>
2018-10-27 13:51:48 +0200

    test changes

---
+++ tests/test_localize.py
@@ -0,0 +1,31 @@
+"""
+Some rudimentary tests.
+"""
+
+from picasso import __main__ as main
+
+
+def test_localize():
+	"""
+    Test localization with mle and drift correction
+	"""
+	import argparse
+
+	parser = argparse.ArgumentParser()
+	args = parser.parse_args()
+
+	args.files = "./data/testdata.raw"
+	args.fit_method = "mle"
+	args.box_side_length = 7
+	args.gradient = 5000
+	args.baseline = 0
+	args.sensitivity = 1
+	args.gain = 1
+	args.qe = 1
+	args.drift = 100
+
+	for fit_method in ['mle']:
+	    args.fit_method = fit_method
+	    main._localize(args)
+
+test_localize()
