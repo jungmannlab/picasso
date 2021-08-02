3c258f2fb599dfefe0ff14504dce3c07c9584c87
Maximilian Strauss <straussmaximilian@googlemail.com>
2021-08-02 12:26:41 +0200

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="picasso",
-    version="0.3.1",
+    version="0.3.2",
     author="Joerg Schnitzbauer, Maximilian T. Strauss",
     author_email=(
         "joschnitzbauer (at) gmail.com, straussmaximilian (at) gmail.com"
