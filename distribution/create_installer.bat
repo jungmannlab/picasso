a542f45b25960050b473b04c3a16a246ee4e3557
Maximilian Strauss <straussmaximilian@googlemail.com>
2021-08-02 12:24:13 +0200

    FIX create installer

--- distribution/create_installer.bat
+++ distribution/create_installer.bat
@@ -8,11 +8,11 @@ call conda activate picasso_installer
 call pip install pyinstaller==4.2
 cd %~dp0\..
 call pip install -r requirements.txt
-python setup.py install
+call python setup.py install
 cd %~dp0
 pyinstaller -y --hidden-import=h5py.defs --hidden-import=h5py.utils  --hidden-import=h5py.h5ac --hidden-import=h5py._proxy --hidden-import=sklearn.neighbors.typedefs --hidden-import=sklearn.neighbors.quad_tree --hidden-import=sklearn.tree --hidden-import=sklearn.tree._utils --hidden-import=scipy._lib.messagestream -n picasso picasso-script.py
 pyinstaller -y --hidden-import=h5py.defs --hidden-import=h5py.utils  --hidden-import=h5py.h5ac --hidden-import=h5py._proxy --hidden-import=sklearn.neighbors.typedefs --hidden-import=sklearn.neighbors.quad_tree --hidden-import=sklearn.tree --hidden-import=sklearn.tree._utils --hidden-import=scipy._lib.messagestream --noconsole -n picassow picasso-script.py
 copy dist\picassow\picassow.exe dist\picasso\picassow.exe
 copy dist\picassow\picassow.exe.manifest dist\picasso\picassow.exe.manifest
 "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" picasso.iss
-call deactivate
+call conda deactivate
