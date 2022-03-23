0bd46545f26b7f6c04021b8a306db984aefadb13
Maximilian Strauss <straussmaximilian@gmail.com>
2022-03-23 11:17:59 +0000

    FEAT new release scheme

---
+++ release/one_click_windows_gui/create_installer_windows.bat
@@ -0,0 +1,26 @@
+call DEL /F/Q/S build > NUL
+call DEL /F/Q/S dist > NUL
+call RMDIR /Q/S build
+call RMDIR /Q/S dist
+
+call cd %~dp0\..\..
+
+call conda create -n picasso_installer python=3.8 -y
+call conda activate picasso_installer
+
+call python setup.py sdist bdist_wheel
+
+call cd release/one_click_windows_gui
+call pip install "../../dist/picassosr-0.3.9-py3-none-any.whl"
+
+call pip install pyinstaller==4.2
+call pyinstaller ../pyinstaller/picasso.spec -y
+call pyinstaller ../pyinstaller/picassow.spec -y
+call conda deactivate
+
+call robocopy ../../picasso dist/picasso/picasso /E
+
+copy dist\picassow\picassow.exe dist\picasso\picassow.exe
+copy dist\picassow\picassow.exe.manifest dist\picasso\picassow.exe.manifest
+
+call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" picasso_innoinstaller.iss
