2f285fad607a5c117a691b3e05650d85c25f1bdf
rafalkowalewski1 <rafalkowalewski998@gmail.com>
2023-02-17 15:29:28 +0100

    Bump version: 0.5.7 → 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Maximilian T. Strauss"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.5.7"
+release = "0.6.0"
 
 # -- General configuration ---------------------------------------------------
 
