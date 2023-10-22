87cb5c8a00367229996ef22044f15af091eb0f08
rafalkowalewski1 <rafalkowalewski998@gmail.com>
2023-10-22 15:26:54 +0200

    Bump version: 0.6.4 → 0.6.5

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Maximilian T. Strauss"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.6.4"
+release = "0.6.5"
 
 # -- General configuration ---------------------------------------------------
 
--- picasso/__version__.py
+++ picasso/__version__.py
@@ -1,1 +1,1 @@
-VERSION_NO = "0.6.4"
+VERSION_NO = "0.6.5"
