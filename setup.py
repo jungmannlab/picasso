ab4c719206807d326cae39d51838e1f1065f0c1d
rafalkowalewski1 <rafalkowalewski998@gmail.com>
2024-09-19 17:03:08 +0300

    pyimariswriter correct version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     entry_points={
         "console_scripts": ["picasso=picasso.__main__:main"],
     },
-    install_requires=requirements + ["PyImarisWriter==0.7.1; sys_platform=='win32'"],
+    install_requires=requirements + ["PyImarisWriter==0.7.0; sys_platform=='win32'"],
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
