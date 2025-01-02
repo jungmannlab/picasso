294f62daf51ab4698370b78e51f2b2ea1ebd8a21
Philippe CARL <philippe.carl@unistra.fr>
2025-01-02 19:44:45 +0100

    Update localize.py
    I erase my changes in this file as requested

--- picasso/localize.py
+++ picasso/localize.py
@@ -344,6 +344,7 @@ def _cut_spots(movie, ids, box):
         return spots
     else:
         """Assumes that identifications are in order of frames!"""
+        
         r = int(box / 2)
         N = len(ids.frame)
         spots = _np.zeros((N, box, box), dtype=movie.dtype)
@@ -354,11 +355,11 @@ def _cut_spots(movie, ids, box):
 
 def _to_photons(spots, camera_info):
     spots = _np.float32(spots)
-    baseline = camera_info["baseline"]
-    sensitivity = camera_info["sensitivity"]
-    gain = camera_info["gain"]
+    baseline = camera_info["Baseline"]
+    sensitivity = camera_info["Sensitivity"]
+    gain = camera_info["Gain"]
     # since v0.6.0: remove quantum efficiency to better reflect precision
-    # qe = camera_info["qe"]
+    # qe = camera_info["Qe"]
     return (spots - baseline) * sensitivity / (gain)
 
 
