d66cd73b4c55ef2c243604b14726e80150f76994
rafalkowalewski1 <rafalkowalewski998@gmail.com>
2025-03-19 14:02:15 +0100

    FIX

--- picasso/gui/render.py
+++ picasso/gui/render.py
@@ -9201,7 +9201,6 @@ class View(QtWidgets.QLabel):
 
         self.update_scene()
 
-    @check_pick
     def save_picked_locs(self, path, channel):
         """ 
         Saves picked locs from a given channel to path as a .hdf5 file. 
