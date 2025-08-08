Fix 3D rotation window for a polygon pick

picasso/postprocess.py
@@ -2105,7 +2105,7 @@ def align(
     
     Returns
     -------
-    locs : np.recarray
+    locs : list of np.recarrays
         Aligned localizations with the shifts applied to the 'x' and
         'y' coordinates.
     """