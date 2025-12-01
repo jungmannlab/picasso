51e84d01199039d50c19555b414ef5f5e9c3b6ba
rafalkowalewski1 <rafalkowalewski998@gmail.com>
2025-12-01 16:50:17 +0100

    Bump version: 0.8.10 → 0.9.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Maximilian T. Strauss"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "0.8.10"
+release = "0.9.0"
 
 # -- General configuration ---------------------------------------------------
 
