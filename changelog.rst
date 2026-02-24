db99300d11492d045513c07eb7660ab4c5665d6a
rafalkowalewski1 <rafalkowalewski998@gmail.com>
2026-02-24 17:14:11 +0100

    PyImarisWriter is included in the one-click-installer again

--- changelog.rst
+++ changelog.rst
@@ -6,6 +6,7 @@ Last change: 24-FEB-2026 CEST
 0.9.8
 -----
 - Added a function ``picasso.lib.get_save_filename_ext_dialog`` that can also check for the existence of the files with other extenstions (for example, if the user tries to save a .yaml file with the same name as an existing .hdf5 file, it will ask if the user wants to overwrite the .hdf5 file). This is implemented in all GUI modules when saving files.
+- ``PyImarisWriter`` is included in the one-click-installer again (Windows only)
 
 0.9.7
 -----
--- release/one_click_windows_gui/create_installer_windows.bat
+++ release/one_click_windows_gui/create_installer_windows.bat
@@ -7,23 +7,18 @@ call cd %~dp0\..\..
 
 call conda create -n picasso_installer python=3.10.19 -y
 call conda activate picasso_installer
-
 call pip install build
 call python -m build
 
+call pip install "dist/picassosr-0.9.7-py3-none-any.whl"
 call cd release/one_click_windows_gui
-call pip install "../../dist/picassosr-0.9.7-py3-none-any.whl"
 
 call pip install pyinstaller==6.19.0
-call pyinstaller ../pyinstaller/picasso.spec -y --clean
-call pyinstaller ../pyinstaller/picassow.spec -y --clean
+call pyinstaller "../pyinstaller/picasso_pyinstaller.py" --onedir --collect-all picasso --collect-all PyImarisWriter --name picasso --icon "../logos/localize.ico" --noconfirm
+call pyinstaller "../pyinstaller/picasso_pyinstaller.py" --onedir --windowed --collect-all picasso --collect-all PyImarisWriter --name picassow --icon "../logos/localize.ico" --noconfirm
+
 call conda deactivate
 call conda remove -n picasso_installer --all -y
 
-call robocopy ../../picasso dist/picasso/_internal/picasso /E
-call robocopy ../../picasso dist/picassow/_internal/picasso /E
-
 copy dist\picassow\picassow.exe dist\picasso\picassow.exe
-copy dist\picassow\picassow.exe.manifest dist\picasso\picassow.exe.manifest
-
 call "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" picasso_innoinstaller.iss
